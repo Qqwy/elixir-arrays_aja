d8bb9cbc94029ce65d086863dd2978fb09d14d7f
Marten/Qqwy <w-m@wmcode.nl>
2021-09-09 12:50:41 +0200

    Adds Benchmark.

---
+++ benchmarks/benchmarks.exs
@@ -0,0 +1,295 @@
+defmodule Benchmarks do
+  @warmup 0.5
+  @time 0.5
+  @memory_time 0.5
+  @parallel 1
+
+  @inputs (
+  (5..20)
+  |> Enum.map(&Integer.pow(2, &1))
+  |> Enum.map(&(1..&1))
+  |> Enum.map(fn range ->
+    name = range.last |> Integer.to_string |> String.pad_leading(10, "0")
+    {"#{name} elements", range}
+  end)
+  )
+
+  def run_benchmarks() do
+    concat_benchmark()
+    random_access_benchmark()
+    random_update_benchmark()
+    append_benchmark()
+  end
+
+  def concat_benchmark do
+    Benchee.run(
+      %{
+        "Arrays.concat/2 (MapArray)" =>
+        {fn input ->
+          Arrays.concat(input.lhs, input.rhs)
+        end,
+         before_each: fn range ->
+           %{range: range,
+             lhs: range |> Enum.shuffle() |> Arrays.new(implementation: Arrays.Implementations.MapArray),
+             rhs: range |> Enum.shuffle() |> Arrays.new(implementation: Arrays.Implementations.MapArray)
+            }
+         end
+        },
+        "Arrays.concat/2 (ErlangArray)" =>
+      {fn input ->
+        Arrays.concat(input.lhs, input.rhs)
+      end,
+       before_each: fn range ->
+         %{range: range,
+           lhs: range |> Enum.shuffle() |> Arrays.new(implementation: Arrays.Implementations.ErlangArray),
+           rhs: range |> Enum.shuffle() |> Arrays.new(implementation: Arrays.Implementations.ErlangArray)
+          }
+       end
+      },
+        "Arrays.concat/2 (A.Vector)" =>
+      {fn input ->
+        Arrays.concat(input.lhs, input.rhs)
+      end,
+       before_each: fn range ->
+         %{range: range,
+           lhs: range |> Enum.shuffle() |> Arrays.new(implementation: A.Vector),
+           rhs: range |> Enum.shuffle() |> Arrays.new(implementation: A.Vector)
+          }
+       end
+      },
+        "Kernel.++/2 (list)" =>
+      {fn input ->
+        input.lhs ++ input.rhs
+      end,
+       before_each: fn range ->
+         %{range: range,
+           lhs: range |> Enum.shuffle(),
+           rhs: range |> Enum.shuffle()
+          }
+       end
+      }
+      },
+      after_each: fn _ -> :erlang.garbage_collect() end, # make garbage collection unlikely to occur _during_ benchmark.
+      inputs: @inputs,
+      warmup: @warmup,
+      time: @time,
+      memory_time: @memory_time,
+      parallel: @parallel,
+      pre_check: true,
+      formatters: [
+        Benchee.Formatters.Console,
+        {Benchee.Formatters.CSV, file: "benchmark_runs/concat.csv"},
+        {Benchee.Formatters.HTML, file: "benchmark_runs/concat.html", auto_open: false},
+        {Benchee.Formatters.Markdown, file: "benchmark_runs/concat.md", description: """
+        Comparing `Arrays.concat` with `Kernel.++`,
+        by concatenating two collections of the same size.
+        """
+        }
+      ]
+    )
+  end
+
+  def random_access_benchmark() do
+    Benchee.run(
+      %{
+        "Arrays.get/2 (MapArray)" =>
+        {fn input ->
+          Arrays.get(input.array, input.index)
+        end,
+         before_scenario: fn range ->
+           %{range: range, array: Arrays.new(range, implementation: Arrays.Implementations.MapArray)}
+         end,
+         before_each: fn input ->
+           Map.put(input, :index, :rand.uniform(input.range.last) - input.range.first)
+         end
+        },
+        "Arrays.get/2 (ErlangArray)" =>
+      {fn input ->
+        Arrays.get(input.array, input.index)
+      end,
+       before_scenario: fn range ->
+         %{range: range, array: Arrays.new(range, implementation: Arrays.Implementations.MapArray)}
+       end,
+       before_each: fn input ->
+         Map.put(input, :index, :rand.uniform(input.range.last) - input.range.first)
+       end
+      },
+        "Arrays.get/2 (A.Vector)" =>
+      {fn input ->
+        Arrays.get(input.array, input.index)
+      end,
+       before_scenario: fn range ->
+         %{range: range, array: Arrays.new(range, implementation: A.Vector)}
+       end,
+       before_each: fn input ->
+         Map.put(input, :index, :rand.uniform(input.range.last) - input.range.first)
+       end
+      },
+        "Enum.fetch/2 (list)" =>
+      {fn input ->
+        Enum.fetch!(input.list, input.index)
+      end,
+       before_scenario: fn range ->
+         %{range: range, list: Enum.into(range, [])}
+       end,
+       before_each: fn input ->
+         Map.put(input, :index, :rand.uniform(input.range.last) - input.range.first)
+       end
+      }
+      },
+      after_each: fn _ -> :erlang.garbage_collect() end, # make garbage collection unlikely to occur _during_ benchmark.
+      inputs: @inputs,
+      warmup: @warmup,
+      time: @time,
+      parallel: @parallel,
+      pre_check: true,
+      # memory_time: @memory_time,
+      formatters: [
+        Benchee.Formatters.Console,
+        {Benchee.Formatters.CSV, file: "benchmark_runs/random_access.csv"},
+        {Benchee.Formatters.HTML, file: "benchmark_runs/random_access.html", auto_open: false},
+        {Benchee.Formatters.Markdown, file: "benchmark_runs/random_access.md", description: """
+        Compares random element access (for reading).
+
+        For arrays, `Arrays.get(myarray, index)` function can be used (myarray[index] will behave similarly as it uses `get` internally.)
+
+        For lists, `Enum.fetch(list, index)` is used.
+        """
+        }
+      ]
+    )
+  end
+
+  def random_update_benchmark() do
+    Benchee.run(
+      %{
+        "Arrays.replace/3 (MapArray)" =>
+        {fn input ->
+          Arrays.replace(input.array, input.index, input.value)
+        end,
+         before_scenario: fn range ->
+           %{range: range, array: Arrays.new(range, implementation: Arrays.Implementations.MapArray)}
+         end,
+        },
+        "Arrays.replace/3 (ErlangArray)" =>
+      {fn input ->
+        Arrays.replace(input.array, input.index, input.value)
+      end,
+       before_scenario: fn range ->
+         %{range: range, array: Arrays.new(range, implementation: Arrays.Implementations.MapArray)}
+       end,
+      },
+        "Arrays.replace/3 (A.Vector)" =>
+      {fn input ->
+        Arrays.replace(input.array, input.index, input.value)
+      end,
+       before_scenario: fn range ->
+         %{range: range, array: Arrays.new(range, implementation: A.Vector)}
+       end,
+      },
+        "List.replace_at/3" =>
+      {fn input ->
+        List.replace_at(input.list, input.index, input.value)
+      end,
+       before_scenario: fn range ->
+         %{range: range, list: Enum.into(range, [])}
+       end,
+      }
+      },
+      before_each: fn input ->
+        input
+        |> Map.put(:index, :rand.uniform(input.range.last) - input.range.first)
+        |> Map.put(:value, :rand.uniform())
+      end,
+      after_each: fn _ -> :erlang.garbage_collect() end, # make garbage collection unlikely to occur _during_ benchmark.
+      inputs: @inputs,
+      warmup: @warmup,
+      time: @time,
+      parallel: @parallel,
+      pre_check: true,
+      # memory_time: @memory_time,
+      formatters: [
+        Benchee.Formatters.Console,
+        {Benchee.Formatters.CSV, file: "benchmark_runs/random_update.csv"},
+        {Benchee.Formatters.HTML, file: "benchmark_runs/random_update.html", auto_open: false},
+        {Benchee.Formatters.Markdown, file: "benchmark_runs/random_update.md", description: """
+        Compares random element replacement.
+
+        For arrays, we check `Arrays.replace/3` as well as Access' `put_in`.
+        These are similar but slightly different APIs for element replacement.
+
+        For lists, `List.replace_at(list, index)` is used.
+        """
+        }
+      ]
+    )
+  end
+
+  def append_benchmark do
+    Benchee.run(
+      %{
+        "Arrays.append/2 (MapArray)" =>
+        {fn input ->
+          Arrays.append(input.collection, input.value)
+        end,
+         before_each: fn input ->
+             Map.put(input, :collection, input.range |> Enum.shuffle |> Arrays.new(implementation: Arrays.Implementations.MapArray))
+         end
+        },
+        "Arrays.append/2 (ErlangArray)" =>
+      {fn input ->
+        Arrays.append(input.collection, input.value)
+      end,
+       before_each: fn input ->
+           Map.put(input, :collection, input.range |> Enum.shuffle |> Arrays.new(implementation: Arrays.Implementations.ErlangArray))
+       end
+      },
+        "Arrays.append/2 (A.Vector)" =>
+      {fn input ->
+        Arrays.append(input.collection, input.value)
+      end,
+       before_each: fn input ->
+         Map.put(input, :collection, input.range |> Enum.shuffle |> Arrays.new(implementation: A.Vector))
+       end
+      },
+        "list ++ [val] (list)" =>
+      {fn input ->
+        input.collection ++ [input.value]
+      end,
+       before_each: fn input ->
+         Map.put(input, :collection, input.range |> Enum.shuffle |> Enum.into([]))
+       end
+      },
+        "[val | list] (list, backwards)" =>
+      {fn input ->
+        [input.value | input.collection]
+      end,
+       before_each: fn input ->
+           Map.put(input, :collection, input.range |> Enum.shuffle |> Enum.into([]))
+       end
+      }
+      },
+      before_each: fn range ->
+        %{range: range, value: :rand.uniform(range.last)}
+      end,
+      after_each: fn _ -> :erlang.garbage_collect() end, # make garbage collection unlikely to occur _during_ benchmark.
+      inputs: @inputs,
+      warmup: @warmup,
+      time: @time,
+      memory_time: @memory_time,
+      parallel: @parallel,
+      pre_check: true,
+      formatters: [
+        Benchee.Formatters.Console,
+        {Benchee.Formatters.CSV, file: "benchmark_runs/append.csv"},
+        {Benchee.Formatters.HTML, file: "benchmark_runs/append.html", auto_open: false},
+        {Benchee.Formatters.Markdown, file: "benchmark_runs/append.md", description: """
+        Comparing `Arrays.append` with appending a value to a list.
+        """
+        }
+      ]
+    )
+  end
+end
+
+Benchmarks.run_benchmarks()
--- mix.exs
+++ mix.exs
@@ -24,7 +24,17 @@ defmodule ArraysAja.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:arrays, "~> 2.0"},
-      {:aja, "~> 0.5.3"}
+      {:aja, "~> 0.5.3"},
+
+      {:ex_doc, "~> 0.23", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.13", only: [:test]},
+
+      {:benchee, "~> 1.0", only: [:dev]},
+      {:benchee_csv, "~> 1.0", only: [:dev]},
+      {:benchee_markdown, "~> 0.2", only: [:dev]},
+      {:benchee_html, "~> 1.0", only: [:dev]}
     ]
   end
 end
